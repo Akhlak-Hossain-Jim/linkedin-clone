c73405dcc37ef0f5b16d6e404b6c48adf393fdbf
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-05-19 23:18:15 +0600

    may 19,2021

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -28,6 +28,77 @@ function Main() {
           </button>
         </div>
       </ShareBox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a href="dl">
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+              <button>
+                <img src="/images/ellipsis.svg" alt="" />
+              </button>
+            </a>
+          </SharedActor>
+          <Description>discription</Description>
+          <SharedImg>
+            <a href="093">
+              <img src="" alt="" />
+            </a>
+          </SharedImg>
+          <SocialCount>
+            <li>
+              <button>
+                <img
+                  src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb"
+                  alt=""
+                />
+                <img
+                  src="https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f"
+                  alt=""
+                />
+                <span>57</span>
+              </button>
+            </li>
+            <li>
+              <a href="ajs">2 comment</a>
+            </li>
+          </SocialCount>
+          <SocialActions>
+            <button>
+              <img
+                className="support"
+                src="/images/like-outline-button.svg"
+                alt=""
+              />
+              <span>Like</span>
+            </button>
+            <button>
+              <img
+                className="support"
+                src="/images/comment-outline-button.svg"
+                alt=""
+              />
+              <span>Comment</span>
+            </button>
+            <button>
+              <img
+                className="support"
+                src="/images/share-outline-arrow.svg"
+                alt=""
+              />
+              <span>Share</span>
+            </button>
+            <button>
+              <img className="support" src="/images/paper-plane.svg" alt="" />
+              <span>Send</span>
+            </button>
+          </SocialActions>
+        </Article>
+      </div>
     </Container>
   );
 }
@@ -68,7 +139,9 @@ const ShareBox = styled(CommonCard)`
     &:first-child {
       display: flex;
       align-items: center;
-      padding: 8px 16px 0;
+      padding: 9.375px 7.5px 9.375px 16px !important;
+      margin: 4px 0 !important;
+      font-size: 13.125px !important;
       img {
         width: 48px;
         border-radius: 50%;
@@ -93,6 +166,139 @@ const ShareBox = styled(CommonCard)`
         img {
           margin: 0 4px 0 -2px;
         }
+      }
+    }
+  }
+`;
+
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 8px;
+  overflow: visible;
+  .support {
+    fill: #5e5e5e !important;
+  }
+`;
+
+const SharedActor = styled.div`
+  padding-right: 40px;
+  display: flex;
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  text-decoration: none;
+  position: relative;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    color: #00000099;
+    font-weight: 600;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 13.125px;
+          color: #000000e6;
+          font-weight: 700;
+        }
+        &:nth-child(n + 1) {
+          font-size: 12px;
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+    img {
+      width: 20px;
+    }
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 15px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SocialCount = styled.ul`
+  list-style: none;
+  line-height: 1.3;
+  display: flex;
+  align-items: start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    a {
+      color: #00000099 !important;
+      font-size: 12px;
+    }
+    button {
+      display: flex;
+      background: transparent;
+      border: none;
+      outline: none;
+      span {
+        color: #00000099 !important;
+        font-size: 12px;
+      }
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 0;
+  min-height: 40px;
+  padding: 4px 8px;
+  button {
+    display: inline-flex;
+    align-items: center;
+    padding: 8px;
+    color: #5e5e5e;
+    font-weight: 700;
+    @media (min-width: 768px) {
+      span {
+        margin-left: 8px;
       }
     }
   }
